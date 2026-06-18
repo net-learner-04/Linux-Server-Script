39c39167a397810f7e6f6eb1dd2789e75bab7e73
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-06-18 23:34:55 +0900

    Implement LVM checks and file system creation functions
    Added functions for checking mount status, resetting physical volumes, and creating file systems.

--- lvm.py
+++ lvm.py
@@ -4,11 +4,13 @@ import os, sys, pathlib, psutil, json
 FILE_SYSTEM = "xfs"
 COMMAND = "xfs_growfs"
 
+
 def root_check():
     '''A function to check if the program is running with root privileges'''
     if os.getuid() != 0:
         print("Run as root")
         sys.exit(1)
+
 
 def all_device_and_lvm_check():
     '''A function that checks the list of all device blocks in the current system and
@@ -26,6 +28,7 @@ def all_device_and_lvm_check():
     
     return target
 
+    
 def mount_status_check(disk_list):
     '''A function that checks whether the disk is currently mounted
       or is a primary disk in use by the operating system'''
@@ -39,6 +42,7 @@ def mount_status_check(disk_list):
 
     return none_mounted_devices
 
+
 def pv_reset(device_list):
     '''Function to initialize a PV (Physical Volume) for LVM configuration'''
     if len(device_list) < 1:
@@ -51,6 +55,7 @@ def pv_reset(device_list):
         except sub.CalledProcessError as e:
             print(f"Failed to pvcreate: {device}  {e}")
             sys.exit(1)
+
 
 def vg_create_or_extend(device_list):
     '''Functions that generate or extend VG (Volume Group)'''
@@ -70,6 +75,7 @@ def vg_create_or_extend(device_list):
         sub.run(["vgextend", f"{vg_name}", *device_list])
         return vg_name
 
+
 def allocate_lv(vg_name):
     '''Functions that allocate LV (Logical Volume)'''
     result = sub.run(["lvs", "--json"], capture_output=True, text=True)
@@ -86,6 +92,7 @@ def allocate_lv(vg_name):
         sub.run(["lvextend", "-l", "+100%FREE", f"/dev/{vg_name}/{lv_name}"])
         return f"/dev/{vg_name}/{lv_name}", False
 
+
 def create_file_system(lv_path, create):
     '''Function to create a file system (XFS) on a created LV (Logical Volume)'''
     # If you want to change the file system, modify the FILE_SYSTEM and COMMAND global variables.
@@ -93,6 +100,7 @@ def create_file_system(lv_path, create):
         sub.run([f"mkfs.{FILE_SYSTEM}", lv_path], check=True)
     else:
         sub.run([COMMAND, lv_path], check=True)
+
 
 def mounting(lv_path, vg_name):
     '''A function that creates a mount point and 
@@ -111,6 +119,7 @@ def mounting(lv_path, vg_name):
         with pathlib.Path("/etc/fstab").open(mode="a", encoding="utf-8") as f:
             f.write(f"\n{fstab_entry}\n")
 
+
 def start():
     root_check()
     AVAILABLE_DEVICE_LIST = mount_status_check(all_device_and_lvm_check())
@@ -119,6 +128,7 @@ def start():
     LV_PATH, CREATE = allocate_lv(VG_NAME)
     create_file_system(LV_PATH, CREATE)
     mounting(LV_PATH, VG_NAME)
+
 
 if __name__ == "__main__":
     start()

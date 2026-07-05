24023e0a66d77c49ea76c98221104c6246e48acd
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-07-05 14:59:30 +0900

    Implement boot time check and update functions
    Added boot time check functionality to monitor system reboots.

--- monitoring/alert.py
+++ monitoring/alert.py
@@ -1,10 +1,11 @@
 import logging as log
 import datetime as dt
-import requests, os, dotenv, sys
-
+import requests, os, dotenv, sys, psutil
+from config import BOOTFILE
+from pathlib import Path
 from config import ALERTFILE, COOLDOWN
 
-dotenv.load_dotenv()
+dotenv.load_dotenv(Path(__file__).parent / ".env")
 
 
 def last_alert_check():
@@ -57,3 +58,23 @@ def send_message(message):
             log.error(f"Discord notification send failed  (status code: {req.status_code})")
     except requests.exceptions.RequestException as e:
         log.error(f"Discord request exception occurred: {e}")
+
+
+def boot_time_check():
+    '''A function that detects whether the current boot time has changed
+    from the reboot time stored in a file.'''
+    boot_time = dt.datetime.fromtimestamp(psutil.boot_time())
+
+    if not os.path.exists(BOOTFILE):
+        return True
+    
+    with open(BOOTFILE, mode="r") as file:
+        last_boot = dt.datetime.fromisoformat(file.read().strip())
+        
+    return last_boot != boot_time
+
+
+def update_boot_time():
+    '''A function that saves the current boot time to the '.last_boot' file.'''
+    with open(BOOTFILE, mode="w") as file:
+        file.write(f"{dt.datetime.fromtimestamp(psutil.boot_time()).isoformat()}\n")

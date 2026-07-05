f3e3898cf3d3df339c3e592453593b719eac67ec
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-07-05 15:00:40 +0900

    Implement root privilege check in main.py
    Added root privilege check function to ensure script runs with necessary permissions.

--- monitoring/main.py
+++ monitoring/main.py
@@ -1,14 +1,18 @@
 import logging as log
-import os
-
-import logger_setup
-from utils import root_check
-from boot import boot_time_check, update_boot_time
+import os, sys
+import monitoring.logger as logger
+from alert import boot_time_check, update_boot_time
 from alert import last_alert_check, update_last_alert, discord_format, send_message
-from checks.system_checks import server_status_check
-from checks.service_checks import service_status_check
-
+from checks import server_status_check, service_status_check
 from config import BOOTFILE
+
+
+def root_check():
+    '''A function to check if the program is running with root privileges'''
+    if os.geteuid() != 0:
+        log.critical("Root privilege required to run this script.")
+        print("Run as root.")
+        sys.exit(os.EX_NOPERM)
 
 
 def start():

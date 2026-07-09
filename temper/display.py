27cb6309a201ed74434966320e4e95cc19817565
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-07-09 20:40:59 +0900

    Change temperature graph representation to block bars

--- temper/display.py
+++ temper/display.py
@@ -1,36 +1,34 @@
 from rich.table import Table
+from rich import box
 from logger import read_log
 
-BRAILLE = ["⠀", "⡀", "⣀", "⣄", "⣤", "⣦", "⣶", "⣷", "⣿"]
+BLOCKS = ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"]
 
 def draw_graph(dev_temps, width=30):
     '''Receives a list of temperatures 
-    and returns a graph string converted to Braille.'''
+    and returns a graph string converted to block bars.'''
     if not dev_temps:
         return "No data."
     temps = [temp for _, temp in dev_temps[-width:]]
     min_temp, max_temp = min(temps), max(temps)
     if min_temp == max_temp:
-        return BRAILLE[-1] * len(temps)
+        return BLOCKS[len(BLOCKS)//2] * len(temps)
     
     result = ""
     for temp in temps:
-        index = round((temp - min_temp) / (max_temp - min_temp) * (len(BRAILLE) - 1))
-        result += BRAILLE[index]
+        index = round((temp - min_temp) / (max_temp - min_temp) * (len(BLOCKS) - 1))
+        result += BLOCKS[index]
     return result
-
 
 def render(dev_temps):
     '''Return the overall device temperature and graph in a 'Rich Table' format.'''
     table = Table(title="Thermometer", 
-                  title_justify="left", 
-                  title_style="bold white"
-                  box=rich.box.ROUNDED)
-
+                  title_justify="center", 
+                  title_style="bold white",
+                  box=box.ROUNDED)
     table.add_column("Dev_name", justify="right", no_wrap=True)
     table.add_column("Current_temp", justify="center")
     table.add_column("Graph")
-
     for device, temp in dev_temps.items():
         dev_name = device.replace(" ", "_").replace("/dev/", "")
         try:
@@ -40,5 +38,4 @@ def render(dev_temps):
         
         graph = draw_graph(log_data)
         table.add_row(dev_name, str(temp), graph)
-
     return table

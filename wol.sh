bba92e0b81d2e1bcc62c99461bea6ed97d82aa67
NetRunner <128007180+net-learner-04@users.noreply.github.com>
2026-08-25 23:25:28 +0900

    Replace broadcast IP with target IP in wol.sh

--- wol.sh
+++ wol.sh
@@ -4,7 +4,7 @@
 TARGET_MAC=""
 
 # The IP address of the target desktop or the broadcast IP address.
-BROADCAST_IP="255.255.255.255"
+TARGET_IP=""
 
 # Log File Path.
 LOG_FILE="/var/log/wol_auto.log"
@@ -19,7 +19,7 @@ if ! command -v wakeonlan &> /dev/null; then
 fi
 
 # Sending WOL Packets and Logging.
-wakeonlan -i "$BROADCAST_IP" "$TARGET_MAC" > /dev/null 2>&1
+wakeonlan -i "$TARGET_IP" "$TARGET_MAC" > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then
     echo "[$TIMESTAMP] WOL packet sent successfully. (MAC: $TARGET_MAC)" >> "$LOG_FILE"
